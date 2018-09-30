9557e657b14b6b0309ecec85c10386cdf4598892
Vinod Kumar <vkviswa@gmail.com>
2018-09-30 22:13:09 +0530

    create args for query fields

--- server.js
+++ server.js
@@ -28,11 +28,19 @@ const StudentType = new gql.GraphQLObjectType({
     lastName: { type: gql.GraphQLString },
     active: { type: gql.GraphQLBoolean },
 
-    // TODO: make the courses field require an argument
+    // make the courses field require an argument
     // called 'level' which filters out the courses array
     // based on that value. Use the parent (root) data in the
     // resolve function to get access to the proper data
-    courses: { ... }
+    courses: { type: new gql.GraphQLList(CourseType),
+              
+              args: {
+                level: {type: new gql.GraphQLNonNull(gql.GraphQLString)}
+              },
+              resolve(student, {level}) {
+                return student.courses.filter(course => course.level === level)
+              }
+            }
   }
 });
 
@@ -53,21 +61,46 @@ const schema = new gql.GraphQLSchema({
         }
       },
       
-      // TODO: make this new 'courseById' field search for a
+      // make this new 'courseById' field search for a
       // course based on the specific ID passed as an argument.
       // Use the JavaScript find method to get the result
-      courseById: { ... },
+      courseById: { 
+        type: CourseType,
+        args: {id: {
+          type: new gql.GraphQLNonNull(gql.GraphQLID)
+        }},
+        resolve(_, {id}) {
+          return COURSES.find(course => course.id===id)
+        }
+      },
 
-      // TODO: make this new 'studentById' field search for a
+      // make this new 'studentById' field search for a
       // student based on the specific ID passed as an argument.
       // Use the JavaScript find method to get the result
-      studentById: { ... },
+      studentById: { 
+        type: StudentType,
+        args: {
+          id: {type: new gql.GraphQLNonNull(gql.GraphQLID)}
+        },
+        resolve(_, {id}) {
+          return STUDENTS.find(student => student.id === id)
+        }
+       },
 
-      // TODO: make this new 'searchStudentByName' field search for
+      // make this new 'searchStudentByName' field search for
       // students by last name based on the 'name' argument passed in.
       // Use a RegExp and the JavaScript filter method to look for
       // students that match (or partially match) the name
-      searchStudentsByName: { ... }
+      searchStudentsByName: { 
+        type: new gql.GraphQLList(StudentType),
+        args: {
+          name: {type: new gql.GraphQLNonNull(gql.GraphQLString)}
+        },
+        resolve(_, { name }) {
+          const pattern = new RegExp(name, 'i');
+          return STUDENTS.filter(student => pattern.test(student.lastName))
+        }
+      }
     }
   })
 });
